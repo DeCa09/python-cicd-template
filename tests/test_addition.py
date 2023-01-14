ac000e825f120b1ec58145acc3ea3330e4209870
Demir Catovic <d.catovic9@gmail.com>
2023-01-14 18:35:26 +0100

    [BLACK-BOT] added formatted files

--- tests/test_addition.py
+++ tests/test_addition.py
@@ -4,11 +4,9 @@ from addition_package.add import add
 
 # this decorator allows that all assert statements in a test are executed,
 # even if the first one were to fail
-@pytest.mark.parametrize("summand_1, summand_2, expected_answer", [
-    (3, 5, 8),
-    (-1, 1, 0),
-    (-1, -1, -2),
-    (5, 0, 5)
-])
+@pytest.mark.parametrize(
+    "summand_1, summand_2, expected_answer",
+    [(3, 5, 8), (-1, 1, 0), (-1, -1, -2), (5, 0, 5)],
+)
 def test_add(summand_1, summand_2, expected_answer):
     assert add(summand_1, summand_2) == expected_answer
