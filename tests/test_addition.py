748adedff1c987ed71d2e34153f8d4a24ad9ed20
Demir Catovic <d.catovic9@gmail.com>
2023-01-14 16:51:53 +0100

    added example package to test workflow

---
+++ tests/test_addition.py
@@ -0,0 +1,14 @@
+import pytest
+from addition_package.add import add
+
+
+# this decorator allows that all assert statements in a test are executed,
+# even if the first one were to fail
+@pytest.mark.parametrize("summand_1, summand_2, expected_answer", [
+    (3, 5, 8),
+    (-1, 1, 0),
+    (-1, -1, -2),
+    (5, 0, 5)
+])
+def test_add(summand_1, summand_2, expected_answer):
+    assert add(summand_1, summand_2) == expected_answer
