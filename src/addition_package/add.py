0dd164e9692b4be4337da50c6c12b10939d54268
Demir Catovic <d.catovic9@gmail.com>
2023-02-18 21:58:50 +0100

    remove type info from docstring

--- src/addition_package/add.py
+++ src/addition_package/add.py
@@ -9,8 +9,8 @@ def add(x: int | float, y: int | float) -> float:
 
 
     Args:
-        x (float): Summand 1.
-        y (float): Summand 2.
+        x: Summand 1.
+        y: Summand 2.
 
     Returns:
         The result of adding x and y.
