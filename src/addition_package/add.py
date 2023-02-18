716186bd911287bbd0e6fffb77086f3182650551
Demir Catovic <d.catovic9@gmail.com>
2023-02-18 21:49:34 +0100

    add docstring to add.py function

--- src/addition_package/add.py
+++ src/addition_package/add.py
@@ -1,2 +1,11 @@
 def add(x: float, y: float) -> float:
+    """Print a greeting.
+
+     Args:
+         x (float): Summand 1.
+         y (float): Summand 2.
+
+     Returns:
+         The result of adding x and y. 
+     """
     return x + y
